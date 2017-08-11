4881c57a219a4eec67ebfd1012558632a9dbe845
Andrii Storozhov <andrii.storozhov@nure.ua>
2017-08-11 14:05:36 +0300

    First commit

---
+++ eto_Assert.hpp
@@ -0,0 +1,20 @@
+#ifndef ETO_ASSERT_HPP
+#define ETO_ASSERT_HPP
+
+#ifdef ASSERTIONS_ENABLED
+
+void eto_AssertErrorHandler(const char *message, const char *fileName, unsigned line);
+
+#define ETO_ASSERT(expr) \
+	if (expr) { }    \
+	else {           \
+		eto_AssertErrorHandler(#expr, __FILE__, __LINE__);\
+	}                
+
+#else 
+
+#define ETO_ASSERT(expr) // evaluates to nothing
+
+#endif // ASSERTIONS_ENABLED
+
+#endif // ETO_ASSERT_HPP
