3bf4337959b6801e7634b775aee29b88fba849c6
Andrii Storozhov <debtalen@hotmail.com>
2017-08-30 17:38:04 +0300

    Add FileStream

---
+++ src/util/FileStream.cpp
@@ -0,0 +1,59 @@
+#include <util/FileStream.hpp>
+
+using std::size_t;
+
+using namespace eto;
+
+FileStream::FileStream(const std::string &path, unsigned accessMode)
+	: m_mode(accessMode)
+{
+	std::ios::openmode mode = std::ios::binary;
+	if (m_mode & Read)
+		mode |= std::ios::in;
+	if (m_mode & Write)
+		mode |= std::ios::out;
+	m_fs.open(path, mode);
+
+	m_open = !m_fs.fail();
+
+	if (m_open)
+	{
+		m_fs.seekg(0, std::ios_base::end);
+		m_size = m_fs.tellg();
+		m_fs.seekg(0, std::ios_base::beg);
+	}
+}
+
+FileStream::~FileStream()
+{
+	close();
+}
+
+size_t FileStream::read(void *buffer, size_t count)
+{
+	if (m_open && isReadable())
+	{
+		m_fs.read(static_cast<char*>(buffer), static_cast<std::streamsize>(count));
+		return static_cast<size_t>(m_fs.gcount());
+	}
+	return 0;
+}
+
+size_t FileStream::write(const void *buffer, size_t count)
+{
+	if (m_open && isWritable())
+	{
+		m_fs.write(static_cast<const char*>(buffer), static_cast<std::streamsize>(count));
+		return count;
+	}
+	return 0;
+}
+
+void FileStream::close() 
+{
+	if (m_open)
+	{
+		m_fs.flush();
+		m_fs.close();
+	}
+}
---
+++ tests/util/FileStream-tests.cpp
@@ -0,0 +1,68 @@
+#include <catch/catch.hpp>
+#include <util/FileStream.hpp>
+#include <cstdio>
+
+using namespace eto;
+#define STRINGFY2(X) #X
+#define STRINGFY(X) STRINGFY2(X)
+#define ROOT /home/morgoth/cpp/eto/
+
+TEST_CASE("FileStream is created", "[FileStream]")
+{
+	SECTION("Existing file read only mode") 
+	{
+		FileStream fs(STRINGFY(ROOT) "/assets/tests/fstreamtests.txt");
+		REQUIRE( fs.isOpen() );
+		REQUIRE( fs.isReadable() );
+		REQUIRE( fs.isWritable() == 0 );
+		fs.close();
+	}
+	SECTION("Non-existing file read only mode")
+	{
+		FileStream fs(STRINGFY(ROOT) "/non-existing");
+		REQUIRE( fs.isOpen() == 0 );
+	}
+	SECTION("Existing file read and write mode")
+	{
+		FileStream fs(STRINGFY(ROOT) "/assets/tests/fstreamtests.txt", FileStream::Write | FileStream::Read) ;
+		REQUIRE( fs.isOpen() );
+		REQUIRE( fs.isReadable() );
+		REQUIRE( fs.isWritable() );
+		fs.close();
+	}
+	SECTION("Non-existing file read and write mode")
+	{
+		FileStream fs(STRINGFY(ROOT) "/non-existing", FileStream::Write | FileStream::Read);
+		REQUIRE( fs.isOpen() == 0 );
+	}
+}
+
+TEST_CASE("FileStream is outputted", "[FileStream]")
+{
+	char buf[128];
+	for (int i = 0; i < 128; ++i)
+		buf[i] = rand() % 50;
+	::remove(STRINGFY(ROOT) "/assets/tests/fstreamtests.txt");
+	FileStream fs(STRINGFY(ROOT) "/assets/tests/fstreamtests.txt", FileStream::Write);
+	REQUIRE( fs.isOpen() );
+	SECTION("via write() function")
+	{
+		REQUIRE( fs.write(buf, 128) == 128 );
+	}
+}
+
+TEST_CASE("FileStream is inputted", "[FileStream]")
+{
+	char buf[128];
+	int b = 10;
+	FileStream fs(STRINGFY(ROOT) "/assets/tests/fstreamtests.txt");
+	REQUIRE( fs.isOpen() );
+	SECTION("via read() function")
+	{
+		REQUIRE( fs.read(buf, 128) == 128 );
+	}
+}
+
+#undef ROOT
+#undef STRINGFY
+#undef STRINGFY2
